11388b24f53510f92550ee296a3b4ae482c4b57c
[Nou-fy] <[rakotonantsoina@gmail.com]>
2024-03-11 17:06:24 +0300

    Class DbConnexion Insertion utilisateur

--- Models/DBConnection.cs
+++ Models/DBConnection.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
 namespace MvcTodo.Models
 {
+    
     public class DBConnection
     {
+        public static NpgsqlConnection connectionstring = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString);
+
+        public static void InsererUtilisateur(Utilisateur utilisateur)
+        {
+            var req = $"INSERT INTO public.\"Utilisateur\"(\"NomUtilisateur\", password) VALUES ('{utilisateur.Name}','{utilisateur.Password}');";
+            try
+            {
+                connectionstring.Open();
+                var cmd = new NpgsqlCommand(req, connectionstring);
+                cmd.ExecuteNonQuery();
+                connectionstring.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
