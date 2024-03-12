14d3a53444260ac4c26c8f5483df5ad42fa33540
Rosotiana <rstcdan8@gmail.com>
2024-03-12 13:34:11 +0100

    db

--- Models/DBConnection.cs
+++ Models/DBConnection.cs
@@ -48,5 +48,81 @@ namespace MvcTodo.Models
 
             return hasUser;
         }
+
+        public static List<Tache> listetache(string username)
+        {
+            var req = $"SELECT * FROM public.\"tache\" WHERE username = '{username}'";
+            var taches = new List<Tache>();
+            try
+            {
+                connectionstring.Open();
+                var cmd = new NpgsqlCommand(req, connectionstring);
+                var rd = cmd.ExecuteReader();
+                while (rd.Read())
+                {
+                    var tache = new Tache(rd.GetInt32(0), rd.GetString(1), rd.GetString(2), rd.GetBoolean(3));
+                    taches.Add(tache);
+                }
+                connectionstring.Close();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return taches;
+        }
+
+        public static void AjoutTache(Tache tache)
+        {
+            var req = $"INSERT INTO public.tache(username, description, state)VALUES( '{tache.Username}', '{tache.Desription}', '{tache.State}'); ";
+            try
+            {
+                connectionstring.Open();
+                var cmd = new NpgsqlCommand(req, connectionstring);
+                cmd.ExecuteNonQuery();
+                connectionstring.Close();
+
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        public static void ModifierTache(int idTache, Tache tache)
+        {
+            var req = "UPDATE public.tache SET description= '" + tache.Desription + "' , state= '" + tache.State + "'  WHERE idtache = '" + idTache + "'";
+
+            try
+            {
+                connectionstring.Open();
+                var cmd = new NpgsqlCommand(req, connectionstring);
+                cmd.ExecuteNonQuery();
+                connectionstring.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void SuppresionTache(int tacheid)
+        {
+            var req = $"DELETE FROM public.tache WHERE tacheid = '{tacheid}' ";
+            try
+            {
+                connectionstring.Open();
+                var cmd = new NpgsqlCommand(req, connectionstring);
+                cmd.ExecuteNonQuery();
+                connectionstring.Close();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
     }
 }
